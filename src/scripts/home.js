a7bb92e2af4b755678e751ac35c1ee4688759521
Nappulat <neazuski@gmail.com>
2025-04-26 22:56:08 +0300

    Add responsive functionality to info.html, add last api-request to home.html.

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,2 +1,5 @@
 import "../styles/home.css";
 import "../styles/main.css";
+import { getUserStatus } from "./user-status.js";
+
+getUserStatus();
---
+++ src/scripts/user-status.js
@@ -0,0 +1,35 @@
+import { fetchData } from './fetch';
+
+const getUserStatus = async () => {
+
+  let user_id = localStorage.getItem('user_id');
+
+  const url = `http://localhost:3000/api/kubios/${user_id}`;
+
+  let token = localStorage.getItem('token');
+
+  const options = {
+      headers: {
+          'Authorization': `Bearer ${token}`,
+      }
+  };
+
+  const response = await fetchData(url, options);
+
+  if (response.error) {
+    if (userData.error == 'jwt expired') {
+      localStorage.clear();
+      location.href="index.html";
+      return
+    }
+      console.error('Error:', response.error);
+      return;
+  }
+
+  if (response.message) {
+      console.log(response.message);
+  }
+
+};
+
+export {getUserStatus};
